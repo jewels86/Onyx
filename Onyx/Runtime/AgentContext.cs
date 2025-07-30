0b1daeb622cd09bd80bcdc397f3cbfa6ab79f570
Jewels <jewels286@proton.me>
2025-07-30 15:04:14 -0400

    Refactor AgentContext and enhance Contract structure with generic parameters

--- Onyx/Runtime/AgentContext.cs
+++ Onyx/Runtime/AgentContext.cs
@@ -1,16 +1,6 @@
 ﻿namespace Onyx.Runtime;
 
 public class AgentContext
-{
-    public required Action<Contract> Extend { get; init; }
-    public required Func<Contract, bool> Check { get; init; }
+{ 
     
-    public AgentContext Clone()
-    {
-        return new AgentContext()
-        {
-            Extend = Extend,
-            Check = Check
-        };
-    }
 }
--- Onyx/Runtime/Contract.cs
+++ Onyx/Runtime/Contract.cs
@@ -1,8 +1,9 @@
 ﻿namespace Onyx.Runtime;
 
-public readonly struct Contract
+public readonly struct Contract<T> where T : struct
 {
     public required byte[] OfferingAgent { get; init; }
     public required byte[] ReceivingAgent { get; init; }
     public required object Tag { get; init; }
+    public required T Parameters { get; init; }
 }
