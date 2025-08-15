105cc5130b2a3a29214c30e3cd08ed2adf9eb475
Jewels <jewels286@proton.me>
2025-08-15 16:44:39 -0400

    Add Onyx.Attack.PostCompilation project with obfuscation functionality
    Onyx.Shared v0.0.1
    Onyx.Attack.PostCompilation v0.0.0

---
+++ Onyx.Attack.PostCompilation/Program.cs
@@ -0,0 +1,21 @@
+﻿using Onyx.Shared;
+
+namespace Onyx.Attack.PostCompilation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: Onyx.Attack.PostCompilation <targetPath>");
+            return;
+        }
+        string targetPath = args[0];
+        var assembly = Mono.Cecil.AssemblyDefinition.ReadAssembly(targetPath);
+        
+        Console.WriteLine($"Obfuscating all types and members with attribute [ObfuscateAttribute] in {assembly.Name.Name} {assembly.Name.Version}");
+        
+        Shared.PostCompilation.ObfuscateWithAttribute(assembly);
+    }
+}
--- Onyx.Shared/PostCompilation.cs
+++ Onyx.Shared/PostCompilation.cs
@@ -56,31 +56,6 @@ public static class PostCompilation
 
         return newType;
     }
-    
-    public static void AsmInject(AssemblyDefinition target, MemoryStream compiled)
-    {
-        var source = AssemblyDefinition.ReadAssembly(compiled, new ReaderParameters { ReadSymbols = false});
-        foreach (var module in source.Modules)
-        {
-            foreach (var type in module.Types.Where(t => !t.IsRuntimeSpecialName))
-            {
-                if (type.Name == "<Module>") continue;
-                var importedType = ImportType(type, target.MainModule);
-                target.MainModule.Types.Add(importedType);
-            }
-        }
-    }
-
-    public static void MethodInject(MethodInfo target, MethodInfo? prefix = null, MethodInfo? postfix = null, Harmony? harmony = null)
-    {
-        harmony ??= new Harmony($"onyx-attack-{GeneralUtilities.NewGUID(8, true)}");
-        harmony.Patch(target, prefix: new HarmonyMethod(prefix), postfix: new HarmonyMethod(postfix));
-    }
-
-    public static void MethodInject(string code, MethodInfo target, Harmony? harmony = null)
-    {
-        
-    }
 
     public static string? ResolveAssemblyPath(AssemblyNameReference reference)
     {
@@ -90,59 +65,90 @@ public static class PostCompilation
             .FirstOrDefault(path =>
                 Path.GetFileNameWithoutExtension(path).Equals(reference.Name, StringComparison.OrdinalIgnoreCase));
     }
-
-    public static List<MetadataReference> ExtractReferences(AssemblyDefinition target)
-    {
-        var refs = new List<MetadataReference>();
-        if (!string.IsNullOrEmpty(target.MainModule.FileName)) 
-            refs.Add(MetadataReference.CreateFromFile(target.MainModule.FileName));
-
-        foreach (var reference in target.MainModule.AssemblyReferences)
-        {
-            string? resolved = ResolveAssemblyPath(reference);
-            if (resolved != null && File.Exists(resolved))
-            {
-                refs.Add(MetadataReference.CreateFromFile(resolved));
-            }
-        }
-
-        return refs;
-    }
-
-    public static List<MetadataReference> ExtractReferences(Assembly asm)
-    {
-        var refs = new List<MetadataReference>();
-        if (!string.IsNullOrEmpty(asm.Location) && File.Exists(asm.Location))
-        {
-            refs.Add(MetadataReference.CreateFromFile(asm.Location));
-        }
-
-        foreach (var refAsmName in asm.GetReferencedAssemblies())
-        {
-            Compilation.TempContext tctx = new();
-            try
-            {
-                var refAsm = tctx.LoadFromAssemblyName(refAsmName);
-                if (!refAsm.IsDynamic && !string.IsNullOrEmpty(refAsm.Location))
-                    refs.Add(MetadataReference.CreateFromFile(refAsm.Location));
-            }
-            catch { }
-            tctx.FullUnload();
-        }
-
-        return refs;
-    }
     
-    public static Assembly? GetLoadedAssembly(string name)
+    public static AssemblyDefinition? ResolveAssembly(AssemblyNameReference reference, ReaderParameters? parameters = null)
     {
-        return AppDomain.CurrentDomain.GetAssemblies()
-            .FirstOrDefault(asm => asm.GetName().Name?.Equals(name, StringComparison.OrdinalIgnoreCase) ?? false);
+        var path = ResolveAssemblyPath(reference);
+        if (path is null) return null;
+
+        parameters ??= new ReaderParameters { ReadSymbols = false };
+        return AssemblyDefinition.ReadAssembly(path, parameters);
     }
 
-    public static AssemblyDefinition? GetDefinitionFrom(Assembly asm)
+    public static Dictionary<string, string> Obfuscate(List<IMemberDefinition> defs, Func<string, string>? obfuscateName = null)
     {
-        if (asm.IsDynamic || string.IsNullOrEmpty(asm.Location)) return null;
+        if (obfuscateName == null)
+            obfuscateName = name => GeneralUtilities.NewGUID(16, true);
+        Dictionary<string, string> obfuscatedNames = [];
         
-        return AssemblyDefinition.ReadAssembly(asm.Location);
+        foreach (var def in defs)
+        {
+            if (def.DeclaringType.IsNested || (def is TypeDefinition typeDef && typeDef.IsNested)) 
+                continue; // Skip nested types
+
+            string newName;
+            if (!obfuscatedNames.TryGetValue(def.Name, out newName!))
+            {
+                newName = obfuscateName(def.Name);
+                obfuscatedNames[$"{MemberDefinitionPrefix(def)}{def.Name}"] = newName;
+            }
+            def.Name = newName;
+        }
+        
+        return obfuscatedNames;
+    }
+
+    public static List<TypeDefinition> GetTypesToObfuscate(List<TypeDefinition> types)
+    {
+        return types
+            .Where(t => t.CustomAttributes.Any(a => a.AttributeType.Name == "ObfuscateAttribute"))
+            .ToList();
+    }
+    public static List<IMemberDefinition> GetMembersToObfuscate(List<IMemberDefinition> members)
+    {
+        return members
+            .Where(m => m.CustomAttributes.Any(a => a.AttributeType.Name == "ObfuscateAttribute"))
+            .ToList();
+    }
+
+    public static Dictionary<string, string> ObfuscateWithAttribute(AssemblyDefinition asm,
+        Func<string, string>? obfuscateName = null)
+    {
+        if (obfuscateName == null)
+            obfuscateName = name => GeneralUtilities.NewGUID(16, true);
+        
+        var typesToObfuscate = GetTypesToObfuscate(asm.MainModule.Types.ToList());
+        var membersToObfuscate = GetMembersToObfuscate(
+            asm.MainModule.Types.SelectMany(t => t.Methods.Cast<IMemberDefinition>())
+                .Concat(asm.MainModule.Types.SelectMany(t => t.Fields.Cast<IMemberDefinition>()))
+                .Concat(asm.MainModule.Types.SelectMany(t => t.Properties.Cast<IMemberDefinition>()))
+                .Concat(asm.MainModule.Types.SelectMany(t => t.Events.Cast<IMemberDefinition>())).ToList());
+
+        var obfuscatedNames = Obfuscate(typesToObfuscate.Cast<IMemberDefinition>().Concat(membersToObfuscate).ToList(), obfuscateName);
+        
+        foreach (var type in typesToObfuscate)
+        {
+            asm.MainModule.Types.Remove(type);
+            asm.MainModule.Types.Add(ImportType(type, asm.MainModule));
+        }
+
+        return obfuscatedNames;
+    }
+
+    public static string MemberDefinitionPrefix(IMemberDefinition def)
+    {
+        if (def is TypeDefinition) return "type:";
+        if (def is MethodDefinition) return "method:";
+        if (def is FieldDefinition) return "field:";
+        if (def is PropertyDefinition) return "property:";
+        if (def is EventDefinition) return "event:";
+        return "unknown:";
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct |
+                    AttributeTargets.Method | AttributeTargets.Property |
+                    AttributeTargets.Field | AttributeTargets.Event)]
+    public class ObfuscateAttribute : Attribute
+    {
     }
 }
